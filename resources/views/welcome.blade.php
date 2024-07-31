6af51ddf9878d00b2d7638692ac82f0f092fe9e5
yunariii <yustinaribka239@gmail.com>
2024-07-31 11:33:39 +0700

    update dashboard regis

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,470 @@
-@extends('layout.layout')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>STI | Sarastya Technology Integrata</title>
+    <link rel="icon" type="image/x-icon" href="img/logo_sarastya.jpg">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <style>
+        /* CSS Enhancements */
 
-@section('content')
-      <!-- Content Header (Page header) -->
-      <div class="content-header">
-        <!-- resources/views/dashboard.blade.php, atau file blade lainnya -->
+        .banner {
+            display: flex;
+            background-color: #ffffff;
+            background-size: cover;
+            border-radius: 20px;
+            margin: 5px 5%;
+            padding: 40px;
+            box-shadow: 0 4px 8px 4px rgba(0, 0, 0, 0.1);
+        }
 
-        <div class="container-fluid">
-          <div class="row mb-2">
-            <div class="col-sm-6">
-              <h1 class="m-0"style="color: #3200af;">Dashboard </h1>
-            </div><!-- /.col -->
-            <div class="col-sm-6">
+        .wlc h2 {
+            margin-top: 3%;
+            font-size: 2rem;
+            font-weight: 800;
+            color: rgba(20, 19, 19, 0.923);
+            margin-bottom: 1%;
+        }
 
-            </div><!-- /.col -->
-          </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-      </div>
-      <!-- /.content-header -->
+        .wlc span {
+            color: #365AC2;
+            font-weight: bolder;
+        }
 
-      <!-- Main content -->
-      <section class="content">
-        
-      </section>
-      <!-- /.content -->
-@endsection
+        .wlc {
+            margin-left: 10%;
+            max-width: 40%;
+        }
+
+        .wlc p {
+            color: #666;
+            font-size: 1.2rem;
+            line-height: 1.5;
+        }
+
+        .pict {
+            max-width: 40%;
+            margin-left: 50px;
+        }
+
+        .pict img {
+            width: 70%;
+            margin-left: 20%;
+        }
+
+        section {
+            max-width: 100%;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        .grid {
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(5%, 4fr));
+            height: 100%;
+            gap: 20px;
+            margin: 0px 2%;
+            padding: 17px;
+        }
+
+        .card {
+            background-color: #fff;
+            border-radius: 15px;
+            overflow: hidden;
+            transition: transform 0.3s, box-shadow 0.3s;
+            cursor: pointer;
+            position: relative;
+        }
+
+        .card-overlay {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            opacity: 0;
+            transition: opacity 0.3s;
+        }
+
+        .card-text {
+            position: absolute;
+            top: 80%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            text-align: left;
+            border-radius: 15px;
+            border: 2px solid white;
+            width: 93%;
+            padding: 20px;
+            background-color: rgba(255, 255, 255, 0.849);
+            color: #020202;
+            opacity: 0;
+            transition: opacity 0.3s;
+        }
+
+        .card:hover .card-overlay {
+            opacity: 1;
+        }
+
+        .card:hover .card-text {
+            opacity: 1;
+        }
+
+        .card:hover {
+            transform: translateY(-10px);
+            box-shadow: 0 20px 40px rgba(0, 0, 0, 0.2);
+        }
+
+        .card-content {
+            align-items: center;
+        }
+
+        .card-icon {
+            width: 97%;
+            height: 0.5%;
+            border: 5px solid white;
+            border-radius: 15px;
+        }
+
+        .card p {
+            font-size: 1rem;
+            margin-bottom: 25%;
+            padding: 10px;
+        }
+
+        .card span {
+            font-weight: bold;
+        }
+
+        html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            font-family: 'Poppins', sans-serif;
+            background-color: #d5def7;
+        }
+
+        body {
+            transition: margin-left 0.3s;
+        }
+
+        .sidebar {
+            height: 100%;
+            width: 250px;
+            position: fixed;
+            top: 0;
+            left: -270px;
+            background-color: white;
+            overflow-x: hidden;
+            transition: 0.3s;
+            padding-top: 100px;
+            box-shadow: 1px 0 9px rgba(0, 0, 0, 0.1);
+            z-index: 1000;
+        }
+
+        .sidebar .sidebar-isi {
+            display: block;
+            padding: 0px;
+            height: 100%;
+        }
+
+        .sidebar-isi .list {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+        }
+
+        .list .nav-link {
+            margin-left: 6%;
+            display: flex;
+            align-items: center;
+            padding: 14px 17px;
+            margin-bottom: 2%;
+            border-radius: 5px;
+            text-decoration: none;
+            width: calc(100% - 40px);
+            box-sizing: border-box;
+            position: relative;
+            justify-content: flex-start;
+            transition: background-color 0.3s, color 0.3s;
+        }
+
+        .nav-link .link {
+            font-size: 17px;
+            color: #365AC2;
+            font-weight: 400;
+            transition: color 0.3s;
+        }
+
+        .nav-link .nav-link-act i {
+            padding-right: 10px;
+            font-size: 20px;
+            color: #365AC2;
+            cursor: pointer;
+            transition: color 0.3s;
+        }
+
+        .nav-link:hover {
+            background-color: #365AC2;
+        }
+
+        .nav-link:hover i,
+        .nav-link:hover .link {
+            color: white;
+        }
+
+        .nav-link-act {
+            margin-left: 6%;
+            display: flex;
+            align-items: center;
+            padding: 14px 17px;
+            margin-bottom: 2%;
+            border-radius: 5px;
+            text-decoration: none;
+            width: calc(100% - 40px);
+            box-sizing: border-box;
+            position: relative;
+            justify-content: flex-start;
+            background-color: #365AC2;
+            color: white;
+        }
+
+        .navbar {
+            position: fixed;
+            background-color: white;
+            padding: 0px;
+            margin-bottom: 10%;
+            display: flex;
+            justify-content: flex-end;
+            font-size: 14px;
+            box-shadow: 0 2px 9px rgba(0, 0, 0, 0.2);
+            width: 100%;
+            top: 0;
+            z-index: 900;
+        }
+
+        .navbar p {
+            margin-right: 2%;
+            padding: 0;
+            color: gray;
+        }
+
+        .navbar span {
+            font-weight: 800;
+            color: #365AC2;
+            font-size: 16px;
+        }
+
+        .open-btn {
+            position: fixed;
+            left: 2%;
+            top: 2.5%;
+            cursor: pointer;
+            color: #365AC2;
+            font-size: 20px;
+            font-weight: 600;
+            border: none;
+            transition: 0.3s;
+            z-index: 1001;
+            background: none;
+        }
+
+        .open-btn:hover {
+            color: darkblue;
+        }
+
+        .main-content {
+            width: calc(100% - 270px);
+            height: 100%;
+            flex: 1;
+            margin-top: 5%;
+            margin-left: 10%;
+            transition: margin-left .3s;
+        }
+        .logoutForm {
+            list-style: none;
+            height: 50%;
+            top: 50%;
+
+        }
+        .logout-button {
+            margin-left: 15%;
+            display: flex;
+            text-align : center;
+            padding: 8px 9px;
+            margin-bottom: 2%;
+            border-radius: 5px;
+            text-decoration: none;
+            font-weight: 700;
+            font-size: 15px;
+            width: calc(80% - 40px);
+            box-sizing: border-box;
+            position: relative;
+            top: 100%;
+            background-color: white;
+            color: #c23636;
+            border: 2px solid #c23636;
+            transition: background-color 0.3s, color 0.3s;
+        }
+
+        .logout-button i{
+            font-weight: 700;
+            font-size: 20px;
+            color: #c23636;
+        }
+
+        .logout-button:hover i,
+        .logout-button:hover {
+            background-color: #c23636;;
+            color: aliceblue;
+            font-weight: 700;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar">
+        <p class="p1"><span>{{ \Carbon\Carbon::now()->format('l') }},</span><br>{{ \Carbon\Carbon::now()->format('F j, Y') }}</p>
+    </nav>
+
+    <button class="open-btn" onclick="toggleSidebar()">&#9776; Dashboard</button>
+
+    <div id="sidebar" class="sidebar">
+        <div class="sidebar-isi">
+            <ul class="list">
+                <li>
+                    <a href="/dashboard" class="nav-link-act">
+                        <span class="link"><i class="fa-solid fa-house-chimney"></i>ㅤDashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/profil" class="nav-link">
+                        <span class="link"><i class="fa-solid fa-id-card"></i>ㅤProfile</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/change-password" class="nav-link">
+                        <span class="link"><i class="fa-solid fa-user-shield"></i>ㅤSecurity</span>
+                    </a>
+                </li>
+            </ul>
+            {{-- <li class="logout">
+                <a href="" class="out-link" id="logoutButton">
+                    <span class="link"><i class='bx bx-log-out'></i>Logout</span>
+                </a>
+            </li> --}}
+            <form id="logoutForm" class="logoutForm" method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit" class="logout-button">ㅤ <i class="fa-solid fa-right-from-bracket"></i>ㅤLogout</button>
+            </form>
+            </ul>
+        </div>
+    </div>
+
+    <div id="main-content" class="main-content">
+        @if (session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
+       <h2>ㅤㅤㅤ ㅤ ㅤㅤㅤㅤㅤ ㅤ    </br>
+    </h2>
+        <div class="banner">
+            <div class="wlc">
+                <h2>Selamat Datang di <span>Sarastya Technology Integrata</span></h2>
+                <p>Menyediakan layanan IT dengan berbagai macam solusi teknologi untuk kebutuhan bisnis Anda.</p>
+            </div>
+        <div class="pict">
+            <img src="img/C.jpg">
+        </div>
+    </div>
+    </section>
+
+    <section>
+        <div class="grid">
+            <div class="card">
+                <div class="card-content">
+                    <img src="img/il.jpg" alt="Grafis ilustratif" class="card-icon">
+                    <div class="card-text">
+                        <p><span>Grafis ilustratif </span></br>gambarkan ide, menceritakan cerita, atau menjelaskan konsep.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-content">
+                    <img src="img/anm.jpg" alt="Motion graphics" class="card-icon">
+                    <div class="card-text">
+                        <p><span>Motion graphics</span></br>melibatkan pembuatan gerakan dan ilusi dari gambar statis atau objek untuk menciptakan ilusi gerak.</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-content">
+                    <img src="img/grp.jpg" alt="Komunikasi visual" class="card-icon">
+                    <div class="card-text">
+                        <p><span>Komunikasi visual </span></br>gunakan elemen-elemen seperti gambar, teks, dan warna untuk menciptakan komunikasi yang efektif.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-content">
+                    <img src="img/vid.jpg" alt="Sinematografi digital" class="card-icon">
+                    <div class="card-text">
+                        <p><span>Sinematografi digital</span></br>konten visual bergerak yang bisa berupa animasi, video klip, atau kombinasi dari keduanya.</br></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+    <script>
+        function toggleSidebar() {
+            var sidebar = document.getElementById("sidebar");
+            var mainContent = document.getElementById("main-content");
+
+            if (sidebar.style.left === "0px") {
+                sidebar.style.left = "-270px";
+                mainContent.style.marginLeft = "10%";
+            } else {
+                sidebar.style.left = "0px";
+                mainContent.style.marginLeft = "19%";
+            }
+        }
+    </script>
+{{--
+<script>
+    document.getElementById('logoutForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Mencegah form submit secara default
+
+        const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+        const headers = new Headers();
+        headers.append('X-CSRF-TOKEN', token);
+        headers.append('Content-Type', 'application/json');
+
+        fetch(this.action, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify({})
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert('Logout berhasil!');
+                window.location.href = data.redirect; // Redirect ke halaman login setelah logout
+            } else {
+                alert(data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Terjadi kesalahan saat logout!');
+        });
+    });
+</script> --}}
+</div>
+
+
+</body>
+</html>
