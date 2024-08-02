bbaa1ea0605948cec8f3162ce6858bda1555f2f4
dim5ptr <dimasetya1836@gmail.com>
2024-08-02 10:51:49 +0700

    update

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,13 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="{{asset('img/logo_sti.png')}}" alt="{{config('app.name')}}">
-@else
-{{ $slot }}
-@endif
-</a>
-</td>
+    <td class="header">
+    <a href="{{ $url }}" style="display: inline-block;">
+    @if (trim($slot) === 'Laravel')
+    {{ asset('img/logo_sti.png') }}
+    <img src="{{ asset('img/logo_sti.png') }}" alt="{{ config('app.name') }}" style="max-width: 100px;">
+    @else
+    {{ $slot }}
+    @endif
+    </a>
+    </td>
 </tr>
