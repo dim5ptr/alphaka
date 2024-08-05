1d0ceebe5a91d4b200bd344ee1f59e577b70f8cd
yunariii <yustinaribka239@gmail.com>
2024-08-05 09:56:12 +0700

    push

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,14 +1,12 @@
 @props(['url'])
-
 <tr>
     <td class="header">
-        <a href="{{ $url }}" style="display: inline-block;">
-            @if (trim($slot) === 'Laravel')
-                <img src="{{ asset('img/logo_sti.svg') }}" alt="{{ config('app.name') }}" style="max-width: 100px; height: auto;">
-            @else
-                <h4>Sarastya</h4>
-                {{ $slot }}
-            @endif
-        </a>
+    <a href="{{ $url }}" style="display: inline-block;">
+    @if (trim($slot) === 'Laravel')
+    <img src="{{ asset("img/logo_sti.svg") }}" alt="{{ config('app.name') }}" style="max-width: 100px;">
+    @else
+    {{ $slot }}
+    @endif
+    </a>
     </td>
 </tr>
