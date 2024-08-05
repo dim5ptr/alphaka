f2de0bdab0bdc4d5ceef64ac0d13247c513fd035
yunariii <yustinaribka239@gmail.com>
2024-08-05 09:19:15 +0700

    push img

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,7 +4,7 @@
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
             @if (trim($slot) === 'Laravel')
-                <img src="{{ asset('images/logo.png') }}" alt="{{ config('app.name') }}" style="max-width: 100px; height: auto;">
+                <img src="{{ asset('img/logo_sti.svg') }}" alt="{{ config('app.name') }}" style="max-width: 100px; height: auto;">
             @else
                 <h4>Sarastya</h4>
                 {{ $slot }}
