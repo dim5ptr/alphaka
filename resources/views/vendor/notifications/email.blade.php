2d7e291fd74d6ebc5fbe1a25f090c64d5db288ef
yunariii <yustinaribka239@gmail.com>
2024-08-19 10:30:22 +0700

    update reset password

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,61 +1,24 @@
-<x-mail::message>
-{{-- Logo --}}
-{{-- Greeting --}}
-@if (! empty($greeting))
-# {{ $greeting }}
-@else
-@if ($level === 'error')
-# @lang('Whoops!')
-@else
+@component('mail::message')
 # @lang('Hola!!')
-@endif
-@endif
 
+Anda menerima email ini karena kami menerima permintaan pengaturan ulang kata sandi untuk akun Anda. Tekan tombol di bawah ini untuk mengatur ulang kata sandi.
 
-{{-- Intro Lines --}}
-<br>Anda menerima email ini karena kami menerima permintaan pengaturan ulang kata sandi untuk akun Anda. Tekan tombol di bawah ini untuk mengatur ulang kata sandi.
-
-{{-- Action Button --}}
-@isset($actionText)
-<?php
-    $color = match ($level) {
-        'success', 'error' => $level,
-        default => 'primary',
-    };
-?>
-<x-mail::button :url="$actionUrl" :color="$color">
+@component('mail::button', ['url' => $actionUrl, 'color' => $color])
 {{ $actionText }}
-</x-mail::button>
-@endisset
+@endcomponent
 
-{{-- Outro Lines --}}
 Tautan Pengaturan Ulang Reset Kata Sandi ini akan kedaluwarsa dalam waktu 60 menit. Jika Anda tidak meminta pengaturan ulang kata sandi, abaikan saja pesan ini.
-<br><br>
 
-{{-- Salutation --}}
-@if (! empty($salutation))
-{{ $salutation }}
-@else
-Salam,<br>
+@lang('Salam,'),<br>
 Sarastya Technology
-@endif
 
-{{-- Subcopy --}}
-@isset($actionText)
-<x-slot:subcopy>
+@slot('subcopy')
 @lang(
     "Jika Anda kesulitan mengakses tombol \":actionText\", salin dan tempel URL di bawah ini ke browser web Anda:",
     [
         'actionText' => $actionText,
     ]
 ) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
-</x-slot:subcopy>
-@endisset
+@endslot
 
-{{-- Footer --}}
-<x-slot:footer>
-    <div style="text-align: center; margin-top: 20px;">
-        <p>&copy; {{ date('Y') }} Sarastya. All rights reserved.</p>
-    </div>
-</x-slot:footer>
-</x-mail::message>
+@endcomponent
