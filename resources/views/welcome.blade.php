cd7f1fb5828c4a5026bc2253cdc3e795222f5351
yunariii <yustinaribka239@gmail.com>
2024-09-12 10:27:14 +0700

    update drop inbox

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -245,31 +245,6 @@
             z-index: 900;
         }
 
-        .inbox {
-            padding-left: 5%;
-            border-right: 5px solid #365AC2;
-            width: 3%;
-            height: 40px;
-            margin-top: 0.9%;
-            margin-right: 1.5%;
-            font-size: 1.5rem;
-            justify-content: center;
-            align-content: center;
-            color: #365AC2;
-        }
-
-        .inbox1{
-            transition: color ease-out .3s;
-            color: #365AC2;
-        }
-
-        .inbox1:hover{
-            color: #626981;
-        }
-        .inbox i {
-            cursor: pointer;
-        }
-
         .navbar p {
             margin-right: 2%;
             padding: 0;
@@ -350,7 +325,6 @@
 </head>
 <body>
     <nav class="navbar">
-        <div class="inbox"><a href="/inbox" class="inbox1"><i class="fa-solid fa-inbox"></i></a></div>
         <p class="p1"><span>{{ \Carbon\Carbon::now()->format('l') }},</span><br>{{ \Carbon\Carbon::now()->format('F j, Y') }}</p>
     </nav>
 
