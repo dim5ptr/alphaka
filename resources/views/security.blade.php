be52e31730becb3523441c24429241da42fc1de7
yunariii <yustinaribka239@gmail.com>
2024-09-12 08:15:48 +0700

    update navbar

--- resources/views/security.blade.php
+++ resources/views/security.blade.php
@@ -146,6 +146,7 @@
             position: fixed;
             background-color: white;
             padding: 0px;
+            margin-bottom: 10%;
             display: flex;
             justify-content: flex-end;
             font-size: 14px;
@@ -153,6 +154,31 @@
             width: 100%;
             top: 0;
             z-index: 900;
+        }
+
+        .inbox {
+            padding-left: 5%;
+            border-right: 5px solid #365AC2;
+            width: 3%;
+            height: 40px;
+            margin-top: 0.9%;
+            margin-right: 1.5%;
+            font-size: 1.5rem;
+            justify-content: center;
+            align-content: center;
+            color: #365AC2;
+        }
+
+        .inbox1{
+            transition: color ease-out .3s;
+            color: #365AC2;
+        }
+
+        .inbox1:hover{
+            color: #626981;
+        }
+        .inbox i {
+            cursor: pointer;
         }
 
         .navbar p {
@@ -440,7 +466,7 @@
             .form-input {
                 margin-bottom: 3%;
             }
-            
+
             .inpage .input-group {
                 position: relative;
                 width: 80%;
