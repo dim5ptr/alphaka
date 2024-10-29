c734be1559d98c70329a1e0ea949c95e8d8eccb4
dim5ptr <dimasetya1836@gmail.com>
2024-10-29 10:21:31 +0700

    u

--- routes/web.php
+++ routes/web.php
@@ -77,6 +77,88 @@ Route::get('/data-usage', function () {
 });
 });
 
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [HttpController::class, 'index'])->name('dashboard');
+    Route::get('/maintenance', function () {
+       return view('maintenance');
+    });
+
+            // Rute untuk logout
+            Route::get('/logout', [HttpController::class, 'logout'])->name('logout');
+            // Rute untuk konfirmasi logout
+            Route::get('/confirm-logout', [HttpController::class, 'confirmLogout'])->name('confirm-logout');
+    Route::middleware(['role:2'])->group(function () {
+
+        Route::get('/dashboardadmin', [HttpController::class, 'showdashboardadm'])->name('showdashboardadm');
+        Route::post('/dashboardadmin', [HttpController::class, 'dashboardadm'])->name('dashboardadm');
+
+        Route::get('/userdata', [HttpController::class, 'showuserdata'])->name('showuserdata');
+        Route::post('/userdata', [HttpController::class, 'userdata'])->name('userdata');
+
+        Route::get('/moredetailsadm', [HttpController::class, 'showmoredetailsadm'])->name('showmoredetailsadm');
+        Route::post('/moredetailsadm', [HttpController::class, 'moredetailsadm'])->name('moredetailsadm');
+
+        Route::get('/userrole', [HttpController::class, 'showuserrole'])->name('showuserrole');
+        Route::post('/userrole', [HttpController::class, 'userrole'])->name('userrole');
+
+        Route::get('/access/{role}', [HttpController::class, 'showaccess'])->name('showaccess');
+        Route::post('/access/{role}', [HttpController::class, 'access'])->name('access');
+
+        Route::get('/personaladm', [HttpController::class, 'personaladm'])->name('personaladm');
+        Route::get('/editpersonaladm',  [HttpController::class, 'showeditpersonaladm'])->name('showeditpersonaladm');
+        Route::post('/editpersonaladm', [HttpController::class, 'editpersonaladm'])->name('editpersonaladm');
+
+        Route::get('/securityadm', [HttpController::class, 'showsecurityadm'])->name('showsecurityadm');
+        Route::get('/changepwadm',  [HttpController::class, 'showchangepwadm'])->name('showchangepwadm');
+        Route::post('/changepwadm',  [HttpController::class, 'changepwadm'])->name('changepwadm');
+
+        Route::get('/edituseradm',  [HttpController::class, 'showedituseradm'])->name('showedituseradm');
+        Route::post('/edituseradm', [HttpController::class, 'edituseradm'])->name('edituseradm');
+    });
+
+    Route::middleware(['role:1'])->group(function () {
+        Route::get('/', [HttpController::class, 'index'])->name('dashboard');
+        Route::get('/organization', [HttpController::class, 'showaddorganization'])->name('organization');
+
+        Route::get('/organizations/{token}', [HttpController::class, 'organizationVerify'])->name('organizations');
+        Route::get('/viewsorganization', [HttpController::class, 'organization'])->name('viewsorganization');
+
+        Route::get('/viewsorganization/{token}', [HttpController::class, 'addMemberOrganization'])->name('addMemberView');
+
+        Route::get('/showcreateorganization', [HttpController::class, 'showcreateorganization'])->name('showcreateorganization');
+
+        Route::get('/moredetails/{organization_name}', [HttpController::class, 'showmoredetails'])->name('showmoredetails');
+        // Route::post('/moredetails/{organization_name}', [HttpController::class, 'moredetails'])->name('moredetails');
+
+        Route::post('/addorganization', [HttpController::class, 'addorganization'])->name('addorganization');
+        Route::get('/vieworganization/{organization_name}', [HttpController::class, 'showvieworganization'])->name('showvieworganization');
+        Route::post('/vieworganization', [HttpController::class, 'vieworganization'])->name('vieworganization');
+        Route::get('/editorganization/{organization_name}', [HttpController::class, 'showeditorganization'])->name('showeditorganization');
+        Route::post('/editorganization/{organization_name}', [HttpController::class, 'editorganization'])->name('editorganization');
+        // Route to show the search and add member view
+        Route::get('/organization/{id}/add_member', [HttpController::class, 'showAddMember'])->name('showAddMember');
+        // Route to handle the member search and add functionality
+        Route::post('/organization/{id}/add_member', [HttpController::class, 'addMember'])->name('addMember');
+        Route::post('/organization/add-member-organization', [HttpController::class, 'addMemberOrganization'])->name('addMemberOrganization');
+        Route::post('/organization/get-member-token', [HttpController::class, 'getMemberToken'])->name('getMemberToken');
+        Route::get('/personal', [HttpController::class, 'personal'])->name('personal');
+        Route::get('/editpersonal',  [HttpController::class, 'showeditpersonal'])->name('showeditpersonal');
+        Route::post('/editpersonal', [HttpController::class, 'editpersonal'])->name('editpersonal');
+        Route::post('/gravatar', [HttpController::class, 'gravatar'])->name('gravatar');
+        Route::get('/upload-profile-picture', [HttpController::class, 'showuploadProfilePicture'])->name('show.upload.profile.picture');
+        Route::post('/upload-profile-picture', [HttpController::class, 'uploadProfilePicture'])->name('upload.profile.picture');
+
+        Route::get('/security', [HttpController::class, 'showsecurity'])->name('showsecurity');
+        Route::get('/editpassword',  [HttpController::class, 'showeditpassword'])->name('showeditpassword');
+        Route::post('/editpassword',  [HttpController::class, 'editpassword'])->name('editpassword');
+
+        Route::get('/user-activity',  [HttpController::class, 'showActivityUser'])->name('showactivityuser');
+        Route::post('/user-activity',  [HttpController::class, 'activityUser'])->name('user.activity');
+    });
+});
+
+
 // // Rute-rute yang memerlukan autentikasi
 // Route::middleware(['auth'])->group(function () {
 //     Route::get('/maintenance', function () {
@@ -158,86 +240,6 @@ Route::get('/data-usage', function () {
 //     Route::get('/edituseradm',  [HttpController::class, 'showedituseradm'])->name('showedituseradm');
 //     Route::post('/edituseradm', [HttpController::class, 'edituseradm'])->name('edituseradm');
 // });
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/', [HttpController::class, 'index'])->name('dashboard');
-    Route::get('/maintenance', function () {
-       return view('maintenance');
-    });
-
-            // Rute untuk logout
-            Route::get('/logout', [HttpController::class, 'logout'])->name('logout');
-            // Rute untuk konfirmasi logout
-            Route::get('/confirm-logout', [HttpController::class, 'confirmLogout'])->name('confirm-logout');
-    Route::middleware(['role:2'])->group(function () {
-
-        Route::get('/dashboardadmin', [HttpController::class, 'showdashboardadm'])->name('showdashboardadm');
-        Route::post('/dashboardadmin', [HttpController::class, 'dashboardadm'])->name('dashboardadm');
-
-        Route::get('/userdata', [HttpController::class, 'showuserdata'])->name('showuserdata');
-        Route::post('/userdata', [HttpController::class, 'userdata'])->name('userdata');
-
-        Route::get('/moredetailsadm', [HttpController::class, 'showmoredetailsadm'])->name('showmoredetailsadm');
-        Route::post('/moredetailsadm', [HttpController::class, 'moredetailsadm'])->name('moredetailsadm');
-
-        Route::get('/userrole', [HttpController::class, 'showuserrole'])->name('showuserrole');
-        Route::post('/userrole', [HttpController::class, 'userrole'])->name('userrole');
-
-        Route::get('/access/{role}', [HttpController::class, 'showaccess'])->name('showaccess');
-        Route::post('/access/{role}', [HttpController::class, 'access'])->name('access');
-
-        Route::get('/personaladm', [HttpController::class, 'personaladm'])->name('personaladm');
-        Route::get('/editpersonaladm',  [HttpController::class, 'showeditpersonaladm'])->name('showeditpersonaladm');
-        Route::post('/editpersonaladm', [HttpController::class, 'editpersonaladm'])->name('editpersonaladm');
-
-        Route::get('/securityadm', [HttpController::class, 'showsecurityadm'])->name('showsecurityadm');
-        Route::get('/changepwadm',  [HttpController::class, 'showchangepwadm'])->name('showchangepwadm');
-        Route::post('/changepwadm',  [HttpController::class, 'changepwadm'])->name('changepwadm');
-
-        Route::get('/edituseradm',  [HttpController::class, 'showedituseradm'])->name('showedituseradm');
-        Route::post('/edituseradm', [HttpController::class, 'edituseradm'])->name('edituseradm');
-    });
-
-    Route::middleware(['role:1'])->group(function () {
-        Route::get('/', [HttpController::class, 'index'])->name('dashboard');
-        Route::get('/organization', [HttpController::class, 'showaddorganization'])->name('organization');
-
-        Route::get('/organizations/{token}', [HttpController::class, 'organizationVerify'])->name('organizations');
-        Route::get('/viewsorganization', [HttpController::class, 'organization'])->name('viewsorganization');
-
-        Route::get('/viewsorganization/{token}', [HttpController::class, 'addMemberOrganization'])->name('addMemberView');
-
-        Route::get('/showcreateorganization', [HttpController::class, 'showcreateorganization'])->name('showcreateorganization');
-
-        Route::get('/moredetails/{organization_name}', [HttpController::class, 'showmoredetails'])->name('showmoredetails');
-        // Route::post('/moredetails/{organization_name}', [HttpController::class, 'moredetails'])->name('moredetails');
-
-        Route::post('/addorganization', [HttpController::class, 'addorganization'])->name('addorganization');
-        Route::get('/vieworganization/{organization_name}', [HttpController::class, 'showvieworganization'])->name('showvieworganization');
-        Route::post('/vieworganization', [HttpController::class, 'vieworganization'])->name('vieworganization');
-        Route::get('/editorganization/{organization_name}', [HttpController::class, 'showeditorganization'])->name('showeditorganization');
-        Route::post('/editorganization/{organization_name}', [HttpController::class, 'editorganization'])->name('editorganization');
-        // Route to show the search and add member view
-        Route::get('/organization/{id}/add_member', [HttpController::class, 'showAddMember'])->name('showAddMember');
-        // Route to handle the member search and add functionality
-        Route::post('/organization/{id}/add_member', [HttpController::class, 'addMember'])->name('addMember');
-        Route::post('/organization/add-member-organization', [HttpController::class, 'addMemberOrganization'])->name('addMemberOrganization');
-        Route::post('/organization/get-member-token', [HttpController::class, 'getMemberToken'])->name('getMemberToken');
-        Route::get('/personal', [HttpController::class, 'personal'])->name('personal');
-        Route::get('/editpersonal',  [HttpController::class, 'showeditpersonal'])->name('showeditpersonal');
-        Route::post('/editpersonal', [HttpController::class, 'editpersonal'])->name('editpersonal');
-        Route::post('/gravatar', [HttpController::class, 'gravatar'])->name('gravatar');
-        Route::get('/upload-profile-picture', [HttpController::class, 'showuploadProfilePicture'])->name('show.upload.profile.picture');
-        Route::post('/upload-profile-picture', [HttpController::class, 'uploadProfilePicture'])->name('upload.profile.picture');
-
-        Route::get('/security', [HttpController::class, 'showsecurity'])->name('showsecurity');
-        Route::get('/editpassword',  [HttpController::class, 'showeditpassword'])->name('showeditpassword');
-        Route::post('/editpassword',  [HttpController::class, 'editpassword'])->name('editpassword');
-
-        Route::get('/user-activity',  [HttpController::class, 'showActivityUser'])->name('showactivityuser');
-        Route::post('/user-activity',  [HttpController::class, 'activityUser'])->name('user.activity');
-    });
-});
 
 
 // Route::get('/send-email', [HttpController::class, 'sendEmail']);
