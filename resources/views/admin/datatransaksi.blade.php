3dbf16bc81fd569efac281a8584a0767c9893bb9
yunariii <yustinaribka239@gmail.com>
2024-10-31 10:59:22 +0700

    update view

--- resources/views/admin/datatransaksi.blade.php
+++ resources/views/admin/datatransaksi.blade.php
@@ -112,7 +112,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0" style="color: #0077FF; font-weight: bold;">Transaction List</h1>
+                <h1 class="m-0" style="color: #0077FF; font-weight: bold;">Transaction History  </h1>
             </div>
         </div>
     </div>
