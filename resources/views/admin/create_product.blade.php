94eb407c7e5d34118e3ce7bb731679de5d4e4281
kaitozu <kaitozugraphic123@gmail.com>
2024-11-11 10:50:21 +0700

    Header design create, edit product

--- resources/views/admin/create_product.blade.php
+++ resources/views/admin/create_product.blade.php
@@ -1,11 +1,11 @@
 @extends('admin.layoutadm.layoutadm')
 
 @section('content')
-<div class="content-header">
+<div class="content-header bg-light p-4 shadow-sm rounded" style="margin-bottom: 2%;">
     <div class="container-fluid">
-        <div class="row mb-2">
+        <div class="row mb-2 align-items-center">
             <div class="col-sm-6">
-                <h1 class="m-0" style="color: #0077FF; font-weight: bold;">New Product</h1>
+                <h1 class="m-0" style="color: #0077FF; font-weight: bold; font-size: 2rem;">New Product</h1>
             </div>
         </div>
     </div>
